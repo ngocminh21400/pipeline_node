9f03aaba68c2eccbf8d1a1c9bdf8e0c654b87f31
ngoc minh <ttngocminh214@gmail.com>
2021-11-30 17:05:16 +0700

    no message

--- test/test2.py
+++ test/test2.py
@@ -1,9 +1,9 @@
 import requests
 
-def random_api_status_code_euqal_200():
+def test_random_api_status_code_euqal_200():
      response = requests.get("http://localhost:4000/random")
      assert response.status_code == 200
 
-def random_api_response_less_than_1():
+def test_random_api_response_less_than_1():
     response = requests.get("http://localhost:4000/random")
     assert response.response_body < 1
